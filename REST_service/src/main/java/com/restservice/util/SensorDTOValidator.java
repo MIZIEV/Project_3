22875712890f8552e528fabd45e22b059a7c7b13
Alexandr Mizev <mizzzev@outlook.com>
2022-09-05 21:55:08 +0300

    -created sensorDTO validator

---
+++ REST_service/src/main/java/com/restservice/util/SensorDTOValidator.java
@@ -0,0 +1,33 @@
+package com.restservice.util;
+
+import com.restservice.dto.SensorDTO;
+import com.restservice.services.SensorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+@Component
+public class SensorDTOValidator implements Validator {
+
+    private final SensorService sensorService;
+
+    @Autowired
+    public SensorDTOValidator(SensorService sensorService) {
+        this.sensorService = sensorService;
+    }
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return SensorDTO.class.equals(clazz);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        SensorDTO sensorDTO = (SensorDTO) target;
+
+        if(sensorService.getSensorByName(sensorDTO.getName()).isPresent()){
+            errors.rejectValue("name","","This sensor already exists!!!");
+        }
+    }
+}
