ff72a119fc95e7ed4f1d489257468e0dfc57b637
Alexandr Mizev <mizzzev@outlook.com>
2022-09-09 17:26:15 +0300

    - added new entity special for JSON response with sum of days with rains - added custom query method in repository for getting above data

--- REST_service/src/main/java/com/restservice/repositories/MeasurementsRepository.java
+++ REST_service/src/main/java/com/restservice/repositories/MeasurementsRepository.java
@@ -2,8 +2,14 @@ package com.restservice.repositories;
 
 import com.restservice.models.Measurements;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface MeasurementsRepository extends JpaRepository<Measurements,Integer> {
+
+    @Query("SELECT count (isRain) FROM Measurements where isRain=true")
+    public Integer getAllMeasurementsWithRain();
 }
